9856e8846b71f535ea0280d4419e7e0726b7b56a
goodplayer <meidomx@gmail.com>
2020-06-01 22:50:07 +0800

    add Int8

--- protocol/types.go
+++ protocol/types.go
@@ -1,12 +1,37 @@
 package protocol
 
+import "errors"
+
+var _ErrNoData = errors.New("no data")
+
 type Type interface {
 	FromBytes(buf []byte) (rest []byte, fin bool, err error)
 	ToBytes() []byte
 	RequiredSize() int
 }
 
+var _ Type = new(Int8)
+
 type Int8 struct {
+	fin   bool
+	Value byte
+}
+
+func (i *Int8) FromBytes(buf []byte) (rest []byte, fin bool, err error) {
+	if len(buf) <= 0 {
+		return nil, i.fin, _ErrNoData
+	}
+	i.Value = buf[0]
+	i.fin = true
+	return buf[1:], true, nil
+}
+
+func (i *Int8) ToBytes() []byte {
+	return []byte{i.Value}
+}
+
+func (i *Int8) RequiredSize() int {
+	return 1
 }
 
 type Int16 struct {
