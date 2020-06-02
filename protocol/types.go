2ad09fb8d5408cae8a7d4b68ed6b8d37076399ad
goodplayer <meidomx@gmail.com>
2020-06-02 21:16:33 +0800

    add Int16 and Int32

--- protocol/types.go
+++ protocol/types.go
@@ -11,6 +11,8 @@ type Type interface {
 }
 
 var _ Type = new(Int8)
+var _ Type = new(Int16)
+var _ Type = new(Int32)
 
 type Int8 struct {
 	fin   bool
@@ -19,7 +21,7 @@ type Int8 struct {
 
 func (i *Int8) FromBytes(buf []byte) (rest []byte, fin bool, err error) {
 	if len(buf) <= 0 {
-		return nil, i.fin, _ErrNoData
+		return nil, i.fin, nil
 	}
 	i.Value = buf[0]
 	i.fin = true
@@ -35,9 +37,61 @@ func (i *Int8) RequiredSize() int {
 }
 
 type Int16 struct {
+	Values [2]byte
+	filled byte
+}
+
+func (i *Int16) FromBytes(buf []byte) (rest []byte, fin bool, err error) {
+	bCnt := i.RequiredSize()
+	max := bCnt - int(i.filled)
+	if max > len(buf) {
+		max = len(buf)
+	}
+	for idx := 0; idx < max; idx++ {
+		i.Values[int(i.filled)] = buf[idx]
+	}
+	if int(i.filled) >= bCnt {
+		return buf[max:], true, nil
+	} else {
+		return buf[max:], false, nil
+	}
+}
+
+func (i *Int16) ToBytes() []byte {
+	return i.Values[:]
+}
+
+func (i *Int16) RequiredSize() int {
+	return 2
 }
 
 type Int32 struct {
+	Values [4]byte
+	filled byte
+}
+
+func (i *Int32) FromBytes(buf []byte) (rest []byte, fin bool, err error) {
+	bCnt := i.RequiredSize()
+	max := bCnt - int(i.filled)
+	if max > len(buf) {
+		max = len(buf)
+	}
+	for idx := 0; idx < max; idx++ {
+		i.Values[int(i.filled)] = buf[idx]
+	}
+	if int(i.filled) >= bCnt {
+		return buf[max:], true, nil
+	} else {
+		return buf[max:], false, nil
+	}
+}
+
+func (i *Int32) ToBytes() []byte {
+	return i.Values[:]
+}
+
+func (i *Int32) RequiredSize() int {
+	return 4
 }
 
 type ArrInt8 []Int8
